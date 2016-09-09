b071eaa8f559317eb32b4e29c2db474ad75b7e9a
Nayden Luiz dos Santos Cruz <nayden.luiz@gmail.com>
2016-09-08 23:54:23 -0300

    Create Repositoty
    Example of use ListView

---
+++ ExampleListView/ListViewXamarin/ListViewXamarin/HomePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            //MainListView.ItemsSource = new List<string> {
+            //    "Harry Potter",
+            //    "Herminion",
+            //    "Ron",
+            //    "Moddy",
+            //    "Malfoy"
+            //};
+            MainListView.ItemsSource = new List<Person> {
+                new Person {Name="Harry",Age="19" },
+                new Person {Name="Ron",Age="18" },
+                new Person {Name="Herminione",Age="18" },
+                new Person {Name="Malfoy",Age="18" },
+                new Person {Name="Moddy",Age="40" },
+                new Person {Name="Snape",Age="105" }
+
+            };
+        }
+    }
+}
